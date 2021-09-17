89742fbdce5bf49850dcbdf87e5904a3a64f0f9d
Deepanshu Rautela <deepanshu.mb20@gmail.com>
2021-09-17 23:52:23 +0530

    Add 05.java

--- 1.Logic Building & Problem Solving/day-02/05.java
+++ 1.Logic Building & Problem Solving/day-02/05.java
@@ -5,12 +5,58 @@ N = 5312
 Result = Five Thousand Three Hundred Twelve
 */
 
-class Main{
-	public static String getString(int num){
-		//TODO
+class ConvertToString{
+	
+	static final String[] units = {
+	"", 
+	"One", 
+	"Two", 
+	"Three", 
+	"Four", 
+	"Five", 
+	"Six", 
+	"Seven", 
+	"Eight", 
+	"Nine", 
+	"Ten",
+	"Eleven", 
+	"Twelve", 
+	"Thirteen", 
+	"Fourteen", 
+	"Fifteen",
+	"Sixteen", 
+	"Seventeen", 
+	"Eighteen", 
+	"Nineteen"
+	}; 
+
+	static final String[] tens = {
+		"", 
+		"", 
+		"Twenty",
+		"Thirty", 
+		"Fourty", 
+		"Fifty", 
+		"Sixty", 
+		"Seventy",
+		"Eighty", 
+		"Ninety"
+	}; 
+
+	public static String convert(int num){
+		if(num < 0) return convert(-num); 
+		if(num < 20) return units[num] + " "; 
+		if(num < 100) return tens[num/10] + units[num%10] + " "; 
+		if(num < 1000) return units[num/100] + "Hundred " + convert(num%100); 
+		if(num < 10000) return units[num/1000] + "Thousand " + convert(num%1000); 
+		else return "";
 	}
 
+}
+
+class Main{
 	public static void main(String[] args){
-		//TODO
+		int num = 5399; 
+		System.out.println(ConvertToString.convert(num));
 	}
 }
